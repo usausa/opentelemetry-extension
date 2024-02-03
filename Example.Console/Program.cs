9bb1c0a6b1b4ea67e558e5f09f9a17d382329544
usausa <machi.pon@gmail.com>
2024-02-03 17:28:11 +0900

    Add project

--- Example.Console/Program.cs
+++ Example.Console/Program.cs
@@ -1,7 +1,19 @@
-using Example.Console;
+using OpenTelemetry.Metrics;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services
+    .AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        // TODO
+        //metrics
+        //    .AddHadwareMonitorInstrumentation()
+        //    .AddSendorOmronInstrumentation();
+
+        // http://localhost:9464/metrics
+        metrics.AddPrometheusHttpListener();
+    });
 
 var host = builder.Build();
 host.Run();
